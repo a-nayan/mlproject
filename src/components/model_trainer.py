0935f1ff15bbab7d796afb43f7cfb5f762045984
a-nayan <nayan_amit@yahoo.com>
2023-10-02 23:55:24 -0400

    data transformation model trainer updated

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -60,6 +60,7 @@ class ModelTrainer:
             if best_model_score <0.6:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
+            logging.info(f"Best Model:"+str(best_model)+f" ,Score :" + str(best_model_score))
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
