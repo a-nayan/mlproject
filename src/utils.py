039fb7ae62ac6194cc0abaad7987301658491195
a-nayan <nayan_amit@yahoo.com>
2023-10-05 11:22:54 -0400

    predit pipeline code added & app.py front end created

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 
 def save_object(file_path,obj):
@@ -41,6 +42,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             test_model_score = r2_score(y_test,y_test_pred)
             report[list(models.keys())[i]] = test_model_score
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
